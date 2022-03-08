0636ac3e9f66312f95220ceb49e871f0c7f4ed29
Filip Jansson <filip.jansson@stralfors.se>
2022-03-08 14:05:47 +0100

    Modified powerhshell profile

--- pwsh/posh_profile.ps1
+++ pwsh/posh_profile.ps1
@@ -1,3 +1,3 @@
-Set-PoshPrompt -Theme ~/git/dotfiles/poshtheme_win.omp.json
+Set-PoshPrompt -Theme ~/git/dotfiles/themes/poshtheme_win.omp.json
 Set-Alias -Name rdns -Value Resolve-DnsName
 Import-Module -Name Terminal-Icons
