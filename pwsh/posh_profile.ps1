0074aa1791d3b0b493157e7db5415380fa638e1a
Filip Jansson <filipjansson@outlook.com>
2022-03-13 13:50:36 +0100

    Changed oh-my-posh init in Windows

--- pwsh/posh_profile.ps1
+++ pwsh/posh_profile.ps1
@@ -1,3 +1,3 @@
-Set-PoshPrompt -Theme ~/git/dotfiles/themes/poshtheme_win.omp.json
+oh-my-posh prompt init pwsh --config ~/git/dotfiles/themes/poshtheme_win.omp.json | Invoke-Expression
 Set-Alias -Name rdns -Value Resolve-DnsName
 Import-Module -Name Terminal-Icons
