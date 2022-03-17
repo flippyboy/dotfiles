4f19c2e2b84deb900457a9b0e64e1127b47ba83e
Filip Jansson <filip.jansson@stralfors.se>
2022-03-17 13:07:41 +0100

    Added scope for rdns alias in pwsh

--- pwsh/posh_profile.ps1
+++ pwsh/posh_profile.ps1
@@ -1,3 +1,3 @@
 Set-PoshPrompt -Theme ~/git/dotfiles/themes/poshtheme_win.omp.json
-Set-Alias -Name rdns -Value Resolve-DnsName
+Set-Alias -Name rdns -Value Resolve-DnsName -Scope Global
 Import-Module -Name Terminal-Icons
