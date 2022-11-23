e8f6df2f02bb4557615dac65e97bf7428a6c3199
Filip Jansson <filip.jansson@stralfors.se>
2022-11-23 08:39:50 +0100

    Changed oh-my-posh init in pwsh

--- pwsh/posh_profile.ps1
+++ pwsh/posh_profile.ps1
@@ -1,3 +1,3 @@
-oh-my-posh prompt init pwsh --config ~/git/dotfiles/themes/poshtheme_win.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~/git/dotfiles/themes/poshtheme_win.omp.json | Invoke-Expression
 Set-Alias -Name rdns -Value Resolve-DnsName -Scope Global
 Import-Module -Name Terminal-Icons
