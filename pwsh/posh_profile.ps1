323e7a171a35098d720abf688bb581ea581012dc
Filip Jansson <filip.jansson@stralfors.se>
2023-10-25 13:25:32 +0200

    Added codium alias

--- pwsh/posh_profile.ps1
+++ pwsh/posh_profile.ps1
@@ -1,3 +1,4 @@
 oh-my-posh --init --shell pwsh --config ~/git/dotfiles/themes/poshtheme_win.omp.json | Invoke-Expression
 Set-Alias -Name rdns -Value Resolve-DnsName -Scope Global
+Set-Alias -Name code -Value codium -Scope Global
 Import-Module -Name Terminal-Icons
